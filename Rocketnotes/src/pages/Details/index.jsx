a03551dd4ee45da58262de1c715985f3886140d9
Kim <cid.kim@gmail.com>
2023-05-20 15:16:31 -0600

    styled-components and vscode-styled-components installed

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
-import "./styles.css"
+import { Container } from "./styles"
 
 export function Details () {
 
 
   return (
-    <h1> Xis Salada</h1>
+    <Container>
+      <h1> Xis Salada</h1>
+    </Container>
+    
   )
 }
