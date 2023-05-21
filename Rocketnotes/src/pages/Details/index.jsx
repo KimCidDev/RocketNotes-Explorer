05a693ee6389639acaa1a9571396bd68cb6083c5
Kim <cid.kim@gmail.com>
2023-05-21 12:31:28 -0600

    now really structuring the project as it is on Figma

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -6,11 +6,17 @@ export function Details () {
   
   return (
     <Container>
-      <h1> Xis Salada</h1>
+      {/*<h1> Xis Salada</h1>
 
 
-      <Button />
-      {/*<div class="box"></div>*/}
+      <Button title="Xis Salada com Ovo" loading/>
+      <Button title="Xis Frango sem maionese"/>
+      <Button title="Xis Bacon Egg"/>
+      <Button title="Xis Calabresa"/>
+      <Button title="Xis vegano"/>  
+      <div class="box"></div>*/}
+
+      <Button title="Xis vegano"/>
     </Container> 
 
 
