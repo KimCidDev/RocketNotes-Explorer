337d9bd95f3c750e7058bcd3efc2c487ab970de2
Kim <cid.kim@gmail.com>
2023-05-20 19:51:40 -0600

    last update

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from "./styles"
 
 export function Details () {
-
-
+  
   return (
     <Container>
       <h1> Xis Salada</h1>
+
+
 
       {/*<div class="box"></div>*/}
     </Container>
