1881994463f413a72b975d6a4a80e2d6f81aeb56
Kim <cid.kim@gmail.com>
2023-05-21 11:33:20 -0600

    updating global styles (font, font size, outline). Adding first components

--- Rocketnotes/src/styles/global.js
+++ Rocketnotes/src/styles/global.js
@@ -10,6 +10,14 @@ export default createGlobalStyle`
 body {
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
   color: #fff;
+  -webkit-font-smoothing: antialiased;
+}
+
+body, a, input, textarea {
+  font-family: 'Roboto Slab', serif;
+  font-size: 16px;
+  outline: none;
+
 }
 
 a {
