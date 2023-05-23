cc4bc09fe9d82a178e7d6d4c215ce9bc068692e6
Kim <cid.kim@gmail.com>
2023-05-23 07:28:44 -0600

    profile component created

--- Rocketnotes/src/components/Header/index.jsx
+++ Rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,15 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
   return (
     <Container>
-      
+      <Profile>
+      <img src="http://github.com/kimciddev.png" alt="Photo a 30yo guy with trees around" />
+      <div>
+        <span> Bem Vindo </span>
+        <strong>Kim Cid</strong>
+      </div>
+      </Profile>
     </Container>
   )
 }
--- Rocketnotes/src/components/Header/styles.js
+++ Rocketnotes/src/components/Header/styles.js
@@ -15,3 +15,33 @@ export const Container = styled.header`
 
   //background: red;
 `;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 56px;
+    width: 56px;
+
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      font-size: 14px;
+      color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+
+    strong {
+      font-size: 18px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+`;
