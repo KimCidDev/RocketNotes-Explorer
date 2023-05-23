b1e86d46a1473264b0d59961558ffb95bc606ad2
Kim <cid.kim@gmail.com>
2023-05-23 08:14:12 -0600

    broken Section component. Still working on it.

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container } from './styles';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
 
 
 export function Details () {
@@ -9,6 +10,14 @@ export function Details () {
   return (
     <Container>   
       <Header />
+
+      <Section title="links úteis">
+        <ul>
+          <li>xisSalada</li>
+          <li>xisBacon</li>
+          <li>xisCamarão</li>
+        </ul>
+      </Section>
 
       <Button title="Voltar" />
     </Container> 
