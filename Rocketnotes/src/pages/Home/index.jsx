967e2455954001bbded48c1f5b8d8a8c21e42226
Kim <cid.kim@gmail.com>
2023-05-23 20:34:09 -0600

    working on the 'home'

---
+++ Rocketnotes/src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { Container, Brand, Menu, Search, Content, Newnote } from './styles';
+
+export function Home () {
+
+  <Container>
+    <Brand>
+    <h1>RocketNotes</h1>
+    </Brand>
+
+  <Menu>
+
+  </Menu>
+
+  <Search>
+
+  </Search>
+
+  <Content>
+    
+  </Content>
+
+  </Container>
+
+
+
+
+
+}
---
+++ Rocketnotes/src/pages/Home/styles.js
@@ -0,0 +1,3 @@
+import styled from 'styled-content';
+
+export const Home = styled.div``;
