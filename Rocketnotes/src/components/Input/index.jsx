b235134da81344f3e4b51af3041034b888e30831
Kim <cid.kim@gmail.com>
2023-05-25 13:12:24 -0600

    working on input component

--- Rocketnotes/src/components/Input/index.jsx
+++ Rocketnotes/src/components/Input/index.jsx
@@ -1,9 +1,9 @@
 import { Container } from './styles'
 
-export function Input ({icon, ...rest}) {
+export function Input ({icon: Icon, ...rest}) {
   return (
     <Container>
-
+      {Icon && <Icon size={20}/>}
       <input {...rest} />
 
     </Container>
--- Rocketnotes/src/components/Input/styles.js
+++ Rocketnotes/src/components/Input/styles.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+`;
