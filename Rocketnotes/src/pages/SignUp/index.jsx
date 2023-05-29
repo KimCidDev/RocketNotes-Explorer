e71083145478b835f8a9a41aafce373f6b59d3da
Kim <cid.kim@gmail.com>
2023-05-29 14:50:51 -0600

    SignUp page done

--- Rocketnotes/src/pages/SignUp/index.jsx
+++ Rocketnotes/src/pages/SignUp/index.jsx
@@ -1,18 +1,23 @@
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock, FiUserPlus } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles';
 
-export function SignIn () {
+export function SignUp () {
   return (
     <Container>
-
+            <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p> Aplicação para salvar e gerenciar seus links úteis</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Cadastre-se</h2>
+        <Input 
+        placeholder="nome"
+        type="text"
+        icon={FiUserPlus}/>
+
         <Input 
         placeholder="email"
         type="text"
@@ -28,12 +33,10 @@ export function SignIn () {
         type="text"
         icon={FiMail}/>
 
-        <Button title="Entrar" />
+        <Button title="Tudo pronto!" />
 
-        <a href="#">Criar Conta</a>
+        <a href="#">Voltar para a Home</a>
       </Form>
-
-      <Background />
     </Container>
   )
 }
---
+++ Rocketnotes/src/pages/SignUp/styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import backgroundImg from '../../assets/creative-desk.jpg';
+
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  padding: 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  > h1 {
+    font-size: 48px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > h2 {
+    font-size: 24px;
+    margin-top: 84px;
+    margin-bottom: 24px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 124px;
+    text-decoration: none;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  height: 100vh;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+
+  //background: ;
+  //background-size: 200%;
+  //background-position: center top;
+  //background-repeat: no-repeat;
+`;
