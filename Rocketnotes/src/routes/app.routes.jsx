3e8f53111aa97370704d9ca6709c1f1631c7982f
Kim <cid.kim@gmail.com>
2023-06-02 13:35:42 -0600

    approutes and authroutes working

--- Rocketnotes/src/routes/app.routes.jsx
+++ Rocketnotes/src/routes/app.routes.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route} from 'react-router-dom';
 
-import { SignIn } from '../pages/SignIn';
-import { SignUp } from '../pages/SignUp';
+import { New } from '../pages/New';
+import { Home } from '../pages/Home';
+import { Details } from '../pages/Details';
+import { Profile } from '../pages/Profile';
 
 export function AppRoutes () {
   return (
     <Routes>
-      <Route path='/' element={<SignIn />} />
-      <Route path='/register' element={<SignUp />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/new' element={<New />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/details/:id' element={<Details />} />
+
+
     </Routes>
   )
 
